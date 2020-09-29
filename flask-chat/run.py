3784248b7b761b0e83aa7a01cdfdd56165c91258
Vasileia-Apostolou <vasiliaapostolou@gmail.com>
2020-09-29 16:33:06 +0000

    Added new libraries and created user profiles

--- flask-chat/run.py
+++ flask-chat/run.py
@@ -1,10 +1,48 @@
 import os
-from flask import Flask
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, session
+
 
 app = Flask(__name__)
+app.secret_key = "randomstring123"
+messages = []
 
-@app.route("/")
+
+def add_messages(username, message):
+    """Add messages to the messages list"""
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append("({}) {}: {}".format(now, username, message))
+
+
+def get_all_messages():
+    """Get all the messages and separate them with a br tag"""
+    return "<br>" .join(messages)
+
+
+@app.route("/", methods = ["GET", "POST"])
 def index():
-    return "<h1>Hello There!</h1>"
+    """Main page with instructions"""
+
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+
+    if "username" in session:
+        return redirect(session["username"])
+
+    return render_template("index.html")
+    
+
+
+@app.route('/<username>')
+def user(username):
+    """Display Chat Messages"""
+    return "<h1>Welcome, {0}</h1>{1}". format(username, get_all_messages())
+
+
+@app.route('/<username>/<message>')
+def send_message(username, message):
+    """Create a new message and redirect back to the chat page"""
+    add_messages(username, message)
+    return redirect("/" + username)
 
 app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
