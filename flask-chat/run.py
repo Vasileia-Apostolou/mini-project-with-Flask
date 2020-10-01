5f960fe7bfc512cc24fe41af14a823cf072b73aa
Vasileia-Apostolou <vasiliaapostolou@gmail.com>
2020-10-01 21:23:34 +0000

    Added chat textarea

--- flask-chat/run.py
+++ flask-chat/run.py
@@ -9,20 +9,15 @@ messages = []
 
 
 def add_messages(username, message):
-    """Add messages to the messages list"""
+    """Add messages to the `messages` list"""
     now = datetime.now().strftime("%H:%M:%S")
-    messages.append("({}) {}: {}".format(now, username, message))
+    messages_dict = {"timestamp": now, "from": username, "message": message}
+    messages.append(messages_dict)
 
 
-def get_all_messages():
-    """Get all the messages and separate them with a br tag"""
-    return "<br>" .join(messages)
-
-
-@app.route("/", methods = ["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
     """Main page with instructions"""
-
     if request.method == "POST":
         session["username"] = request.form["username"]
 
@@ -30,19 +25,27 @@ def index():
         return redirect(session["username"])
 
     return render_template("index.html")
-    
 
 
-@app.route('/<username>')
+@app.route("/<username>", methods=["GET", "POST"])
 def user(username):
-    """Display Chat Messages"""
-    return "<h1>Welcome, {0}</h1>{1}". format(username, get_all_messages())
+    """Display chat messages"""
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_messages(username, message)
+        return redirect(session["username"])
+
+    return render_template("chat.html", username=username,
+                           chat_messages=messages)
 
 
-@app.route('/<username>/<message>')
+
+@app.route("/<username>/<message>")
 def send_message(username, message):
     """Create a new message and redirect back to the chat page"""
     add_messages(username, message)
     return redirect("/" + username)
+
 
 app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
